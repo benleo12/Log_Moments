156cfa66b24712c704e11b8f7e6138d1794549ca
Stefan Hoeche <shoeche@fnal.gov>
2024-06-25 11:19:29 -0500

    updated plotting for thrust

--- Logtests/ps/shower_weighted_run_LL.py
+++ Logtests/ps/shower_weighted_run_LL.py
@@ -5,314 +5,8 @@ from vector import Vec4
 from particle import Particle, CheckEvent
 from qcd import AlphaS, NC, TR, CA, CF
 import numpy as np
-
-
-
-class Soft:
-
-    def __init__(self,flavs,Ca):
-        self.flavs = flavs
-        self.Ca = Ca
-
-    def Value(self,z,pi,pj,pk,e):
-        n = -pj-e[0]-e[1]
-        sij = pi.SmallMLDP(pj)
-        sik = pi.SmallMLDP(pk)
-        skj = pk.SmallMLDP(pj)
-        D = sij*(pk*n)+skj*(pi*n)
-        if D == 0: return mn(0)
-        A = 2*sik*(pi*n)/D
-        return self.Ca*A
-    def Estimate(self,z,ip):
-        return self.Ca*4/(-z[1])
-    def Integral(self,ip):
-        return self.Ca*4*mylog(1/ip[-1])
-    def GenerateZ(self,ip):
-        return [mn(1),-mypow(ip[-1],rng.random())]
-
-class Coll:
-
-    def __init__(self,flavs,Ca):
-        self.flavs = flavs
-        self.Ca = Ca
-
-class Cqq (Coll):
-
-    def Value(self,z,pi,pj,pk,e):
-        return self.Ca*(1-z[1])*0
-    def Estimate(self,z,ip):
-        return self.Ca
-    def Integral(self,ip):
-        return self.Ca
-    def GenerateZ(self,ip):
-        return [mn(0),mn(rng.random())]
-
-class Cgg (Coll):
-
-    def Value(self,z,pi,pj,pk,e):
-        return self.Ca*z[1]*(1-z[1])
-    def Estimate(self,z,ip):
-        return self.Ca
-    def Integral(self,ip):
-        return self.Ca
-    def GenerateZ(self,ip):
-        return [mn(0),mn(rng.random())]
-
-class Cgq (Coll):
-
-    def Value(self,z,pi,pj,pk,e):
-        return TR/2*(1-2*z[1]*(1-z[1]))
-    def Estimate(self,z,ip):
-        return TR/2
-    def Integral(self,ip):
-        return 5*TR/2
-    def GenerateZ(self,ip):
-        fl = rng.randint(1,5)
-        self.flavs[1] = fl
-        self.flavs[2] = -fl
-        return [mn(0),mn(rng.random())]
-
-class Shower:
-
-    def __init__(self,alpha,t0,coll,beta,rt0,flat,nmax,lc):
-        self.nmax = nmax
-        self.t0 = t0
-        self.rt0 = rt0
-        self.beta = beta
-        if len(flat)!=2: self.flat = False
-        else:
-            self.flat = True
-            self.lmin = flat[0]
-            self.lmax = flat[1]
-        self.alpha = alpha
-        self.alphamax = alpha(self.t0)
-        self.amode = 0 if self.alpha.order == -1 else 1
-        if self.amode != 0:
-            self.alphamax = (2*m.pi)/self.alpha.beta0(5)
-        self.kernels = {}
-        for fl in [-5,-4,-3,-2,-1,1,2,3,4,5]:
-            self.kernels[fl] = [ Soft([fl,fl,21],CA/2 if lc else CF)  ]
-        self.kernels[21] = [ Soft([21,21,21],CA/2) ]
-        if coll & 1:
-            for fl in [-5,-4,-3,-2,-1,1,2,3,4,5]:
-                self.kernels[fl].append( Cqq([fl,fl,21],CA/2 if lc else CF) )
-        if coll & 2:
-            self.kernels[21].append( Cgg([21,21,21],CA/2) )
-            self.kernels[21].append( Cgq([21,0,0],0) )
-        self.Ca = CA/2 if lc else CF
-        self.Bl = -3./4. if coll&1 else -1.
-
-    def dsigma(self,v):
-        if self.alpha.order >= 0:
-            print('Order \\alpha not supported')
-            exit(1)
-        cx = mylog(v)
-        J = 1/(1+self.beta)
-        aS = self.alpha(self.Q2,5)
-        return myexp(-J*aS*self.Ca/(m.pi)* \
-                     (cx**2/mn(2)-2*self.Bl*(cx+13/mn(4)-30*v)))* \
-            (-J*aS*self.Ca/(m.pi)*(cx-2*self.Bl*(1-30*v)))
-
-    def MakeKinematics(self,x,y,phi,pijt,pkt,moms):
-        Kt = moms[0]+moms[1]
-        v, Q2 = -y, 2*pijt*Kt
-        z, omz = x[0]+x[1], 1-x[1] if x[0] == 0 else -x[1]
-        pk, pi = pkt, pijt*z
-        n, n_perp = Kt+pijt-pi, Rotation(pi,pk).y
-        if n_perp.P2() == 0: return []
-        n_perp *= 1/n_perp.P()
-        l_perp = LT(n,pi,n_perp)
-        if l_perp.M2() == 0: l_perp = pi.Cross(n_perp)
-        l_perp *= 1/mysqrt(abs(l_perp.M2()))
-        kap = Kt.M2()/Q2
-        ktt = v*((1-v)*omz-v*kap)*Q2
-        if ktt < 0: return []
-        ktt = mysqrt(ktt);
-        pj = ktt*(mycos(phi)*n_perp+mysin(phi)*l_perp)
-        pj += omz*pijt+v*(Kt-(omz+2*kap)*pijt)
-        K = Kt+pijt-pi-pj
-        oldcm, newcm = -Kt, -K
-        if oldcm.M2() <= 0 or newcm.M2() <= 0: return []
-        for i,p in enumerate(moms):
-            if i<2: continue
-            moms[i] = oldcm.BoostBack(newcm.Boost(p))
-        pi = oldcm.BoostBack(newcm.Boost(pi))
-        pj = oldcm.BoostBack(newcm.Boost(pj))
-        pk = oldcm.BoostBack(newcm.Boost(pk))
-        return [pi,pj,pk,moms]
-
-    def MakeColors(self,flavs,colij,colk):
-        self.c += 1
-        if flavs[0] != 21:
-            if flavs[0] > 0:
-                return [ [self.c,0], [colij[0],self.c] ]
-            else:
-                return [ [0,self.c], [self.c,colij[1]] ]
-        else:
-            if flavs[1] == 21:
-                if colij[0] == colk[1]:
-                    if colij[1] == colk[0] and rng.random()>0.5:
-                        return [ [colij[0],self.c], [self.c,colij[1]] ]
-                    return [ [self.c,colij[1]], [colij[0],self.c] ]
-                else:
-                    return [ [colij[0],self.c], [self.c,colij[1]] ]
-            else:
-                if flavs[1] > 0:
-                    return [ [colij[0],0], [0,colij[1]] ]
-                else:
-                    return [ [0,colij[1]], [colij[0],0] ]
-
-    def IDParams(self,qa,evt):
-        Kt = -evt[0].mom-evt[1].mom
-        Q2 = 2*qa*Kt
-        kap = Kt.M2()/Q2
-        t0 = self.ct0/Kt.M2()
-        omzp = t0/(1+t0)
-        return [Kt,Q2,kap,omzp]
-
-    def UpdateWeights(self,split):
-        gsum, osum = mn(0), self.gs[split.id][1]
-        self.gs[split.id] = [self.gs[split.id][0],mn(0)]
-        for i,sf in enumerate(self.kernels[split.pid]):
-            id = self.IDParams(split.mom,event)
-            g = self.alphamax/(2*m.pi)*sf.Integral(id)
-            ng = 2 if ( split.col[0]!=0 and split.col[1]!=0 ) else 1
-            self.gs[split.id].append([gsum+ng*g,split.id,ng,i,id])
-            gsum += ng*g
-        self.gs[split.id][1] = gsum
-        for g in self.gs[split.id:]: g[0] += gsum-osum
-
-    def GenerateZ(self,event,momsum,s,t):
-        z = s[2].GenerateZ(s[3])
-        omz = 1-z[1] if z[0] == 0 else -z[1]
-        if omz == 0: return False
-        phi = 2*m.pi*rng.random()
-        Q2, sijt = momsum.M2(), 2*s[0].mom.SmallMLDP(s[1].mom)
-        sit, sjt = 2*momsum*s[0].mom, 2*momsum*s[1].mom
-        if sijt <= 0 or sit >= 0 or sjt >= 0: return False
-        v, rho = mysqrt(t/Q2), mypow(sit*sjt/(Q2*sijt),self.beta/2)
-        Q, a, b = mysqrt(sijt/Q2*sit/sjt), 1, self.beta
-        kt = mysqrt(Q2)*mypow(rho*v,a/(a+b))*mypow(Q*omz,b/(a+b))
-        y = kt**2/(-2*s[0].mom*momsum)
-        y = y/omz
-        evt = [ p.mom for p in event ]
-        moms = self.MakeKinematics(z,y,phi,s[0].mom,s[1].mom,evt)
-        if moms == []: return False
-        if self.amode == 0:
-            w = self.alpha(kt**2,5)/self.alphamax
-        else:
-            asref = self.alpha.asa(t,5)
-            if asref>0: w = self.alpha(kt**2,5)/asref
-            else: w = 0
-        w *= s[2].Value(z,moms[0],moms[1],moms[2],moms[3])/s[2].Estimate(z,s[3])
-        w *= 1/(1+self.beta)
-        if w <= rng.random(): return False
-        for i, j in zip(event,evt): i.mom = j
-        cols = self.MakeColors(s[2].flavs,s[0].col,s[1].col)
-        event.append(Particle(s[2].flavs[2],moms[1],cols[1],len(event),[0,0]))
-        for c in [0,1]:
-            if cols[1][c] != 0 and cols[1][c] == s[0].col[c]:
-                event[-1].cps[c] = s[0].cps[c]
-                event[s[0].cps[c]].cps[1-c] = event[-1].id
-                s[0].cps[c] = 0
-        s[0].Set(s[2].flavs[1],moms[0],cols[0])
-        for c in [0,1]:
-            if cols[0][c] != 0 and cols[0][c] == cols[1][1-c]:
-                event[-1].cps[1-c] = s[0].id
-                s[0].cps[c] = event[-1].id
-        s[1].mom = moms[2]
-        s[2] = event[-1]
-        self.UpdateWeights(s[0])
-        self.gs.append([self.gs[-1][0],mn(0)])
-        self.UpdateWeights(s[2])
-        return True
-
-    def SelectSplit(self,event,rn1,rn2):
-        l, r = 0, len(self.gs)-1
-        c = int((l+r)/2)
-        a, d = self.gs[c][0], rn1*self.gs[-1][0]
-        while r-l > 1:
-            if d < a: r = c
-            else: l = c
-            c = int((l+r)/2)
-            a = self.gs[c][0]
-        if d < self.gs[l][0]: r = l
-        k = r
-        l, r = 2, len(self.gs[k])-1
-        c = int((l+r)/2)
-        a, d = self.gs[k][c][0], rn2*self.gs[k][1]
-        while r-l > 1:
-            if d < a: r = c
-            else: l = c
-            c = int((l+r)/2)
-            a = self.gs[k][c][0]
-        if d < self.gs[k][l][0]: r = l
-        if k != self.gs[k][r][1]: print('Error 1 in integral table')
-        if event[k].cps[0] != 0:
-            if event[k].cps[1] != 0:
-                if self.gs[k][r][2] != 2: print('Error 2 in integral table')
-                s = rng.choice(event[k].cps)
-            else:
-                if self.gs[k][r][2] != 1: print('Error 2 in integral table')
-                s = event[k].cps[0]
-        else:
-            if self.gs[k][r][2] != 1: print('Error 2 in integral table')
-            s = event[k].cps[1]
-        sf = self.kernels[event[k].pid][self.gs[k][r][-2]]
-        return [ event[k], event[s], sf, self.gs[k][r][-1] ]
-
-    def GeneratePoint(self,event):
-        momsum = event[0].mom+event[1].mom
-        trials = 0
-        while self.t > self.ct0:
-            trials += 1
-            if trials == self.nmax:
-                print('Abort after',trials,'trials in rank',comm.Get_rank())
-                self.t = self.ct0
-                return
-            t = self.ct0
-            g = self.gs[-1][0]
-            if self.amode == 0:
-                tt = self.t*mypow(mn(rng.random()),1/g)
-            else:
-                l2 = self.alpha.l2a(5)
-                tt = l2*mypow(self.t/l2,mypow(mn(rng.random()),1/g))
-            if tt > t:
-                t = tt
-                s = self.SelectSplit(event,rng.random(),rng.random())
-            if len(event) == 4 and self.flat:
-                lmax = min(self.lmax,self.alpha(self.Q2,5)/2*mylog(mn(1)/16))
-                lmin = self.lmin
-                lam = lmax+(lmin-lmax)*rng.random()
-                t = self.Q2*myexp(lam/(self.alpha(self.Q2,5)/2))
-                self.weight *= (lmax-lmin)*2/self.alpha(self.Q2,5)
-                self.t = t
-                while True:
-                    if self.GenerateZ(event,momsum,s,t):
-                        self.weight *= self.dsigma(t/self.Q2)
-                        return
-                    s = self.SelectSplit(event,rng.random(),rng.random())
-            self.t = t
-            if self.t > self.ct0:
-                if self.GenerateZ(event,momsum,s,t): return
-
-    def Run(self,event,nem):
-        em = 0
-        self.c = 2
-        self.weight = 1
-        self.ct0 = self.t0
-        self.Q2 = (event[0].mom+event[1].mom).M2()
-        self.t = self.Q2
-        self.gs = [[mn(0),mn(0)],[mn(0),mn(0)]]
-        for split in event[2:]:
-            self.gs.append([self.gs[-1][0],mn(0)])
-            self.UpdateWeights(split)
-        while self.t > self.ct0:
-            if em >= nem: return
-            self.GeneratePoint(event)
-            if em == 0 and self.rt0 != 0:
-                self.ct0 = max(self.t0,self.t*self.rt0)
-            em += 1
+import dire
+import alaric
 
 # build and run the generator
 import sys, time, optparse
@@ -335,6 +29,7 @@ parser.add_option("-C","--cut",default=1,dest="cut")
 parser.add_option("-R","--rcut",default='0',dest="rcut")
 parser.add_option("-Q","--ecms",default='91.1876',dest="ecms")
 parser.add_option("-F","--flat",default='[]',dest="flat")
+parser.add_option("-S","--shower",default='A',dest="shower")
 parser.add_option("-q","--quad",default=0,action="count",dest="quad")
 parser.add_option("-K","--cluster",default=5,dest="cas")
 parser.add_option("-l","--logfile",default="",dest="logfile")
@@ -365,8 +60,12 @@ alphas = AlphaS(ecms,mn(opts.alphas),int(opts.order))
 #if MPI.COMM_WORLD.Get_rank() == 0:
 print("t_0 = {0}, log(Q^2/t_0) = {1}, \\alpha_s(t_0) = {2}". \
           format(t0,mylog(ecms**2/t0),alphas(t0)))
-shower = Shower(alphas,t0,int(opts.coll),mn(opts.beta),
-                mn(opts.rcut),eval(opts.flat),int(opts.nmax),opts.lc)
+if opts.shower == 'A':
+    shower = alaric.Shower(alphas,t0,int(opts.coll),mn(opts.beta),
+                           mn(opts.rcut),eval(opts.flat),int(opts.nmax),opts.lc)
+else:
+    shower = dire.Shower(alphas,t0,int(opts.coll),mn(opts.beta),
+                         mn(opts.rcut),eval(opts.flat),int(opts.nmax),opts.lc)
 jetrat = SimplifiedAnalysis(-0.0033)
 
 #rng.seed((comm.Get_rank()+1)*int(opts.seed))
@@ -401,9 +100,9 @@ min_tau = 10**-24#10
 max_tau = 0.999#0.1
 #coeff = 2 * alpha_s / (3 * np.pi)
 
-alphas = [ AlphaS(91.1876,0.01,0), AlphaS(91.1876,0.01,0) ]
-analytics = nll.NLL(alphas,a=1,b=1,t=91.2**2)
-
+alphas = [ AlphaS(ecms,mn(opts.alphas),0),
+           AlphaS(ecms,mn(opts.alphas),int(opts.order)) ]
+analytics = nll.NLL(alphas,a=1,b=1,t=ecms**2)
 
 # Define wLL and wNLL functions
 
